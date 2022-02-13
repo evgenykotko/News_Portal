cf3774d56fcaf9ba91514fef5e1240b86a4773ec
evgenykotko <kotko.evgeny@gmail.com>
2022-02-14 00:37:35 +0400

    Fixes

--- newsportal/views.py
+++ newsportal/views.py
@@ -6,11 +6,10 @@ class NewsList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
-    # queryset = Post.objects.filter(type_post="news").order_by("-id")
+    queryset = Post.objects.order_by("-id")
 
 
 class NewsPost(DetailView):
     model = Post
     template_name = 'newspost.html'
     context_object_name = 'newspost'
-    queryset = Post.objects.all()
